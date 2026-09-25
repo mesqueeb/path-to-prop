5caf3e70f15e1c8e3f053651a7f9407b74a8bef1
Luca Ban <mesqueeb@users.noreply.github.com>
2026-09-26 10:55:07 +1200

    chore: rebuild dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,16 +1,18 @@
 /**
  * Returns the keys of a path
  *
- * @param   {string} path   a/path/like.this
- * @returns {string[]} with keys
+ * @param {string} path A/path/like.this
+ * @returns {string[]} With keys
  */
 export declare function getKeysFromPath(path: string | string[]): string[];
 /**
- * Gets a deep property in an object, based on a path to that property. Pass the path as array when you have prop names with `.` or `/` in them
+ * Gets a deep property in an object, based on a path to that property. Pass the path as array when
+ * you have prop names with `.` or `/` in them
  *
- * @param {{ [key in string]: unknown }} obj an object to wherefrom to retrieve the deep reference of
+ * @param {{ [key in string]: unknown }} obj An object to wherefrom to retrieve the deep reference
+ *   of
  * @param {string | string[]} path 'path/to.prop' or ['path', 'to', 'prop']
- * @returns {unknown} the last prop in the path
+ * @returns {unknown} The last prop in the path
  */
 export declare function pathToProp(obj: {
     [key in string]: unknown;
--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,20 @@
 /**
  * Returns the keys of a path
  *
- * @param   {string} path   a/path/like.this
- * @returns {string[]} with keys
+ * @param {string} path A/path/like.this
+ * @returns {string[]} With keys
  */
 export function getKeysFromPath(path) {
     return path.split ? path.split(/\.|\//) : path;
 }
 /**
- * Gets a deep property in an object, based on a path to that property. Pass the path as array when you have prop names with `.` or `/` in them
+ * Gets a deep property in an object, based on a path to that property. Pass the path as array when
+ * you have prop names with `.` or `/` in them
  *
- * @param {{ [key in string]: unknown }} obj an object to wherefrom to retrieve the deep reference of
+ * @param {{ [key in string]: unknown }} obj An object to wherefrom to retrieve the deep reference
+ *   of
  * @param {string | string[]} path 'path/to.prop' or ['path', 'to', 'prop']
- * @returns {unknown} the last prop in the path
+ * @returns {unknown} The last prop in the path
  */
 export function pathToProp(obj, path) {
     const keys = getKeysFromPath(path);
